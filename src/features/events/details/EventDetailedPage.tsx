05e50272f83616336c935344bd07a27650a4b311
“tmccann” <tommy001@gmail.com>
2024-05-14 18:25:09 +0100

    start section 5

---
+++ src/features/events/details/EventDetailedPage.tsx
@@ -0,0 +1,5 @@
+export default function EventDetailedPage() {
+  return (
+    <div>EventDetailedPage</div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './app/layouts/App.tsx'
 import 'semantic-ui-css/semantic.min.css'
 import './app/layouts/styles.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/router/Routes.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
